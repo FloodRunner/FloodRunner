0e43502a9d77bbd39b21c0b9cb239e6ce89c4946
Javaad Patel <javaadpatel@gmail.com>
2020-12-25 12:53:56 +0200

    Delete server.js

--- SandboxRunner/server.js
+++
@@ -1,2 +0,0 @@
-//empty server.js file used to activate build on web application SCM_BUILD_DURING_DEPLOYMENT
-//according to this: https://github.com/Microsoft/Oryx/blob/master/doc/runtimes/nodejs.md
