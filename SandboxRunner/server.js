a118c51db9254cb28694490ac23ab18cde884684
javaad <javaadpatel@gmail.com>
2020-12-25 12:41:21 +0200

    Added server.js file to sandboxrunner to activate SCM build Updated Readme

---
+++ SandboxRunner/server.js
@@ -0,0 +1,2 @@
+//empty server.js file used to activate build on web application SCM_BUILD_DURING_DEPLOYMENT
+//according to this: https://github.com/Microsoft/Oryx/blob/master/doc/runtimes/nodejs.md
